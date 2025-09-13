82f8b59657993530d9e25068a29a55d714288b89
Advik <iammackk@gmail.com>
2025-09-13 19:01:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 3.117.1"
     }
   }
   required_version = ">= 1.0.0"
@@ -13,110 +13,108 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "n8nResourceGroup"
-  location = "eastus"
+  name     = "rg-azure-n8n"
+  location = "East US"
 }
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = "n8nVNet"
+  name                = "vnet-azure-n8n"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_subnet" "subnet" {
-  name                 = "n8nSubnet"
+  name                 = "subnet-azure-n8n"
   resource_group_name  = azurerm_resource_group.rg.name
   virtual_network_name = azurerm_virtual_network.vnet.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
+# NIC and VM for vm1
 resource "azurerm_network_interface" "nic1" {
-  name                = "n8nNIC1"
+  name                = "nic1-azure-n8n"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 
   ip_configuration {
-    name                          = "internal"
+    name                          = "IPConfig1"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip1.id
   }
-}
-
-resource "azurerm_network_interface" "nic2" {
-  name                = "n8nNIC2"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pip2.id
-  }
-}
-
-resource "azurerm_public_ip" "pip1" {
-  name                = "n8nPublicIP1"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_public_ip" "pip2" {
-  name                = "n8nPublicIP2"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
 }
 
 resource "azurerm_linux_virtual_machine" "vm1" {
-  name                = "n8nVM1"
+  name                = "vm1-azure-n8n"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  network_interface_ids = [
-    azurerm_network_interface.nic1.id,
-  ]
+
+  admin_username = "azureuser"
+
+  network_interface_ids = [azurerm_network_interface.nic1.id]
+
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("C:\Users\91781\.ssh\id_ed25519.pub")
+    public_key = file("C:/Users/91781/.ssh/id_rsa.pub")
   }
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
+    offer     = "UbuntuServer"
     sku       = "22_04-lts-gen2"
     version   = "latest"
+  }
+
+  computer_name               = "vm1"
+  disable_password_authentication = true
+}
+
+# NIC and VM for vm2
+resource "azurerm_network_interface" "nic2" {
+  name                = "nic2-azure-n8n"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "IPConfig2"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_linux_virtual_machine" "vm2" {
-  name                = "n8nVM2"
+  name                = "vm2-azure-n8n"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   size                = "Standard_B1s"
-  admin_username      = "azureuser"
-  network_interface_ids = [
-    azurerm_network_interface.nic2.id,
-  ]
+
+  admin_username = "azureuser"
+
+  network_interface_ids = [azurerm_network_interface.nic2.id]
+
   admin_ssh_key {
     username   = "azureuser"
-    public_key = file("C:\Users\91781\.ssh\id_ed25519.pub")
+    public_key = file("C:/Users/91781/.ssh/id_rsa.pub")
   }
+
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
   }
+
   source_image_reference {
     publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
+    offer     = "UbuntuServer"
     sku       = "22_04-lts-gen2"
     version   = "latest"
   }
+
+  computer_name               = "vm2"
+  disable_password_authentication = true
 }
